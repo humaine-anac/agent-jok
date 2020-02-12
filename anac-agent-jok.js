d0d426aadeb8ec05bc50f9aad1fe2976cecad15f
Jeff Kephart <kephart@us.ibm.com>
2020-02-12 17:48:13 -0500

    Update to Express 4.

--- anac-agent-jok.js
+++ anac-agent-jok.js
@@ -60,7 +60,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(methodOverride());
-//app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 const getSafe = (p, o, d) =>
