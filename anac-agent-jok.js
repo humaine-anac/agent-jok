8f6aef0bfbc64aa5fe80508c1b16da904a3f14cc
pevelm <pevelm@rpi.edu>
2020-02-19 19:18:16 +0000

    Update package.json dependencies

--- anac-agent-jok.js
+++ anac-agent-jok.js
@@ -58,10 +58,7 @@ const app = express();
 app.set('port', process.env.PORT || myPort);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 app.use(methodOverride());
-app.use(express.static(path.join(__dirname, 'public')));
 
 const getSafe = (p, o, d) =>
   p.reduce((xs, x) => (xs && xs[x] != null && xs[x] != undefined) ? xs[x] : d, o);
