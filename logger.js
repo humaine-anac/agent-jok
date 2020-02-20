44d9d54d230e6c1f473bdeadf0599bc12f8d9bbb
Jeff Kephart <kephart@us.ibm.com>
2020-02-20 15:45:09 -0500

    Get rid of silly test message.

--- logger.js
+++ logger.js
@@ -10,7 +10,7 @@ Logger.setLogLevel = function(level) {
 Logger.logExpression = function (xpr, level) {
   if (level > logLevel) return;
   xpr = (typeof xpr == 'object') ? JSON.stringify(xpr, null, 2) : xpr;
-  console.log('Hey There! [' + moment().format('YYYY-MM-DD hh:mm:ss.SS') + '] ' + xpr);
+  console.log('[' + moment().format('YYYY-MM-DD hh:mm:ss.SS') + '] ' + xpr);
 };
 
 module.exports = Logger;
