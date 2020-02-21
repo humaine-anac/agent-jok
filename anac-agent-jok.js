f7958023f085cd9b4b089d0351527685da8eb9d2
Jeff Kephart <kephart@us.ibm.com>
2020-02-21 11:14:20 -0500

    Add extractBid POST route, and re-factor to create new extractBid.js module.

--- anac-agent-jok.js
+++ anac-agent-jok.js
@@ -12,6 +12,7 @@ let methodOverride = require('method-override');
 let bodyParser = require('body-parser');
 
 const {classifyMessage} = require('./anac-conversation.js');
+const {extractBidFromMessage, interpretMessage} = require('./extractBid');
 
 let myPort = appSettings.defaultPort || 14007;
 let agentName = appSettings.name || "Agent007";
@@ -109,6 +110,31 @@ app.post('/classifyMessage', (req, res) => {
     })
     .catch(err => {
       logExpression("Error from Watson Assistant: ", 2);
+      logExpression(err, 2);
+      res.json(err);
+    });
+  }
+});
+
+// POST API route that simply calls Watson Assistant on the supplied text message to obtain intents and entities
+app.post('/extractBid', (req, res) => {
+  logExpression("Inside extractBid (POST).", 2);
+  if(req.body) {
+    let message = req.body;
+    message.speaker = message.speaker || defaultSpeaker;
+    message.addressee = message.addressee || null;
+    message.role = message.role || message.defaultRole;
+    message.environmentUUID = message.environmentUUID || defaultEnvironmentUUID;
+    logExpression("Message is: ", 2);
+    logExpression(message, 2);
+    return extractBidFromMessage(message)
+    .then(extractedBid => {
+      logExpression("Extracted bid : ", 2);
+      logExpression(extractedBid, 2);
+      res.json(extractedBid);
+    })
+    .catch(err => {
+      logExpression("Error extracting bid: ", 2);
       logExpression(err, 2);
       res.json(err);
     });
@@ -279,124 +305,6 @@ function sendMessage(message) {
   return postDataToServiceType(message, 'anac-environment-orchestrator', '/relayMessage');
 }
 
-// From the intents and entities obtained from Watson Assistant, extract a structured representation
-// of the message
-function interpretMessage(watsonResponse) {
-  logExpression("In interpretMessage, watsonResponse is: ", 2);
-  logExpression(watsonResponse, 2);
-  let intents = watsonResponse.intents;
-  let entities = watsonResponse.entities;
-  let cmd = {};
-  if (intents[0].intent == "Offer" && intents[0].confidence > 0.2) {
-    let extractedOffer = extractOfferFromEntities(entities);
-    cmd = {
-      quantity: extractedOffer.quantity
-    };
-    if(extractedOffer.price) {
-      cmd.price = extractedOffer.price;
-      if(watsonResponse.input.role == "buyer") {
-        cmd.type = "BuyOffer";
-      }
-      else if (watsonResponse.input.role == "seller") {
-        cmd.type = "SellOffer";
-      }
-    }
-    else {
-      if(watsonResponse.input.role == "buyer") {
-        cmd.type = "BuyRequest";
-      }
-      else if (watsonResponse.input.role == "seller") {
-        cmd.type = "SellRequest";
-      }
-    }
-  }
-  else if (intents[0].intent == "AcceptOffer" && intents[0].confidence > 0.2) {
-    cmd = {
-      type: "AcceptOffer"
-    };
-  }
-  else if (intents[0].intent == "RejectOffer" && intents[0].confidence > 0.2) {
-    cmd = {
-      type: "RejectOffer"
-    };
-  }
-  else {
-    cmd = null;
-  }
-  if(cmd) {
-    cmd.metadata = watsonResponse.input;
-    cmd.metadata.addressee = watsonResponse.input.addressee || extractAddressee(entities); // Expect the addressee to be provided, but extract it if necessary
-    cmd.metadata.timeStamp = new Date();
-  }
-  return cmd;
-}
-
-// Extract the addressee from entities (in case addressee is not already supplied with the input message)
-function extractAddressee(entities) {
-  let addressees = [];
-  let addressee = null;
-  entities.forEach(eBlock => {
-    if(eBlock.entity == "avatarName") {
-      addressees.push(eBlock.value);
-    }
-  });
-  logExpression("Found addressees: ", 2);
-  logExpression(addressees, 2);
-  if(addressees.includes(agentName)) addressee = agentName;
-  else addressee = addressees[0];
-  return addressee;
-}
-
-// Extract goods and their amounts from the entities extracted by Watson Assistant
-function extractOfferFromEntities(entityList) {
-  let entities = JSON.parse(JSON.stringify(entityList));
-  let removedIndices = [];
-  let quantity = {};
-  let state = null;
-  let amount = null;
-  entities.forEach((eBlock,i) => {
-    entities[i].index = i;
-    if(eBlock.entity == "sys-number") {
-      amount = parseFloat(eBlock.value);
-      state = 'amount';
-    }
-    else if (eBlock.entity == "good" && state == 'amount') {
-      quantity[eBlock.value] = amount;
-      state = null;
-      removedIndices.push(i-1);
-      removedIndices.push(i);
-    }
-  });
-  entities = entities.filter(eBlock => {
-    return !(removedIndices.includes(eBlock.index));
-  });
-  let price = extractPrice(entities);
-  return {
-    quantity,
-    price
-  };
-}
-
-// Extract price from entities extracted by Watson Assistant
-function extractPrice(entities) {
-  let price = null;
-  entities.forEach(eBlock => {
-    if(eBlock.entity == "sys-currency") {
-      price = {
-        value: eBlock.metadata.numeric_value,
-        unit: eBlock.metadata.unit
-      };
-    }
-    else if(eBlock.entity == "sys-number" && !price) {
-      price = {
-        value: eBlock.metadata.numeric_value,
-        unit: "USD"
-      };
-    }
-  });
-  return price;
-}
-
 // Calculate utility for a given bundle of goods and price, given the utility function
 function calculateUtilityAgent(utilityInfo, bundle) {
   logExpression("In calculateUtilityAgent, utilityParams and bundle are: ", 2);
@@ -531,6 +439,7 @@ function selectMessage(messageSet) {
 // * interpreting the intents and entities into a structured representation of the offer
 // * determining (through self-policing) whether rules permit a response to the offer
 // * generating a bid (or other negotiation act) in response to the offer
+
 function processOffer(message) {
   logExpression("In processOffer, message is: ", 2);
   logExpression(message, 2);
